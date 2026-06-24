650f2c7ca5e65bb07d273913b93b2328713d2652
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-24 19:59:09 +0530

    updated

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const numbers = [1,2,3,4];
-const doubled = numbers.map((num) => {
-  return num * 5;
-})
-console.log(doubled);
+// const numbers = [1,2,3,4];
+// const doubled = numbers.map((num) => {
+//   return num * 5;
+// })
+// console.log(doubled);
 
 const name = ["nihala" ,"amna", "sulu"];
 const upperNames = name.map(name =>
@@ -13,3 +13,15 @@ console.log(upperNames);
 let score = 1;
 score++;
 console.log(score);
+
+const fruits = ["apple", "orange", "grape"];
+fruits.push("mango");
+console.log(fruits);
+
+const numbers = [1,2,3,4];
+const result =
+numbers.map(
+  num => num * 3
+
+);
+console.log(result);
