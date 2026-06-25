af2064c9349cb319131970c4084d02c3df2e09ee
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-25 09:02:22 +0530

    updated

--- script.js
+++ script.js
@@ -18,10 +18,16 @@ const fruits = ["apple", "orange", "grape"];
 fruits.push("mango");
 console.log(fruits);
 
-const numbers = [1,2,3,4];
-const result =
-numbers.map(
-  num => num * 3
+// const numbers = [1,2,3,4];
+// const result =
+// numbers.map(
+//   num => num * 3
 
+// );
+// console.log(result);
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const result = numbers.filter(
+  num => num > 3
 );
 console.log(result);
